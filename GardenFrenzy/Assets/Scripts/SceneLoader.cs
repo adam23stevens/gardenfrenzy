de6f34db1bb3e572bee2a4d539fcdfefeb02fed6
adam23stevens <adam23stevens@gmail.com>
2020-09-25 16:59:47 +0100

    garden frenzy update

---
+++ GardenFrenzy/Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    [SerializeField]
+    public int LoadingTimeSeconds;
+
+    private int _sceneIndex;
+
+    void Start()
+    {
+        _sceneIndex = SceneManager.GetActiveScene().buildIndex;
+
+        if (_sceneIndex == 0)
+        {
+            StartCoroutine(HandleLoadSceneToMenu());
+        }
+    }
+
+    IEnumerator HandleLoadSceneToMenu()
+    {
+        yield return WaitRoutine(LoadingTimeSeconds);
+        LoadNextScene();
+    }
+
+    private IEnumerator WaitRoutine(int waitTimeSeconds)
+    {
+        yield return new WaitForSeconds(waitTimeSeconds);
+    }
+
+    public void LoadNextScene()
+    {
+        SceneManager.LoadScene(_sceneIndex + 1);
+    }
+}
